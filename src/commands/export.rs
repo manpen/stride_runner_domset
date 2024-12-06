16c9e855a437103624237ab4c536c075cd621867
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2024-12-06 22:53:50 +0100

    Use caching for export-instance subcommand

--- src/commands/export.rs
+++ src/commands/export.rs
@@ -1,9 +1,13 @@
-use std::path::Path;
+use std::{io::Write, path::Path};
 
 use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
+use sqlx::SqlitePool;
 
 use crate::utils::{
-    download_progress_bar::DownloadProgressBar, server_connection::ServerConnection,
+    directory::StrideDirectory,
+    download_progress_bar::DownloadProgressBar,
+    instance_data_db::{IId, InstanceDataDB},
+    server_connection::ServerConnection,
 };
 
 use super::arguments::{CommonOpts, ExportSolutionOpts, ImportInstanceOpts};
@@ -44,19 +48,44 @@ async fn download(
     Ok(())
 }
 
+// TODO: de-duplicate this code
+async fn open_db_pool(path: &Path) -> anyhow::Result<SqlitePool> {
+    if !path.is_file() {
+        anyhow::bail!("Database file {path:?} does not exist. Run the >update< command first");
+    }
+
+    let pool = sqlx::sqlite::SqlitePool::connect(
+        format!("sqlite:{}", path.to_str().expect("valid path name")).as_str(),
+    )
+    .await?;
+    Ok(pool)
+}
+
 pub async fn command_export_instance(
     common_opts: &CommonOpts,
     cmd_opts: &ImportInstanceOpts,
 ) -> anyhow::Result<()> {
+    let stride_dir = StrideDirectory::try_default()?;
     let server_conn = ServerConnection::new_from_opts(common_opts)?;
-    let search_path = format!("api/instances/download/{}", cmd_opts.instance);
-    download(
-        server_conn,
-        &search_path,
-        cmd_opts.output.as_path(),
-        cmd_opts.force,
-    )
-    .await
+    let instance_data_db = InstanceDataDB::new(stride_dir.db_instance_file().as_path()).await?;
+    let meta_db = open_db_pool(stride_dir.db_meta_file().as_path()).await?;
+    let data = instance_data_db
+        .fetch_data(&server_conn, &meta_db, IId(cmd_opts.instance))
+        .await?;
+
+    let destination = cmd_opts.output.as_path();
+    if !cmd_opts.force && cmd_opts.output.exists() {
+        anyhow::bail!(
+            "File already exists: {}; change output path or use -f/--force to overwrite",
+            destination.display()
+        );
+    }
+
+    let mut file = std::fs::File::create(destination)?;
+    file.write_all(data.as_bytes())?;
+
+    println!("Stored instance data to: {}", destination.display());
+    Ok(())
 }
 
 pub async fn command_export_solution(
